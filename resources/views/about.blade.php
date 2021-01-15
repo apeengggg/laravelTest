7e64a6ae0feacd4d2c21890f9cde198aeaf1d819
apeengggg <mirfanmanaf0804@gmail.com>
2021-01-15 16:09:58 +0700

    give some comment for the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,8 @@
+{{-- Get the layout html code ON folder layout/main.blade.php --}}
 @extends('layout.main')
+{{-- get the tittle on the layout with the user tittle input 'About Me' or 'lorem' --}}
 @section('tittle', 'About Me')
+{{-- insert the container division on the layout page and bring here --}}
 @section('container')
 <div class="container">
     <div class="row">
@@ -8,4 +11,5 @@
         </div>
     </div>
 </div>
+{{-- end section for container section --}}
 @endsection
